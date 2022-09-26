725fee59bd64d70763b65e4d6604165b10e10274
chtholine <oleg123qwe@gmail.com>
2022-09-26 09:08:00 +0300

    First Docker commit ✨

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ from faker import Faker
 fake = Faker()
 
 
-def info_generator():
-    print(
-        f"Hello, my name is {fake.name()}!\nI was born {fake.date()}.\n"
-        f"I live at {fake.address()}.\nHave a nice day!"
-    )
-
-
-if __name__ == "__main__":
-    info_generator()
+print(
+    f"Hello, my name is {fake.name()}!\nI was born {fake.date()}.\n"
+    f"I live at {fake.address()}.\nHave a nice day!"
+)
