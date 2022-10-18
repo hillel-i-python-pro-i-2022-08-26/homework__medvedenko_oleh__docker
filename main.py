7d2773c377f73fcb5e10e24fb19ba80fee944891
chtholine <oleg123qwe@gmail.com>
2022-10-18 19:34:30 +0300

    Data_organizer ✨

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ class DataProvider:
     _faker: Faker = field(default_factory=Faker)
 
     def _generate_group_names(
-            self,
-            amount: int = 10,
+        self,
+        amount: int = 10,
     ) -> T_GROUP_NAMES:
         return [self._faker.unique.company() for _ in range(amount)]
 
@@ -33,7 +33,7 @@ class DataProvider:
         )
 
     def _generate_humans(
-            self, groups: T_GROUP_NAMES, amount_of_humans: int
+        self, groups: T_GROUP_NAMES, amount_of_humans: int
     ) -> T_HUMANS:
         members = []
         for _ in range(amount_of_humans):
@@ -44,9 +44,9 @@ class DataProvider:
         return members
 
     def generate_group_members(
-            self,
-            amount_of_groups: None | int = None,
-            amount_of_humans: None | int = None,
+        self,
+        amount_of_groups: None | int = None,
+        amount_of_humans: None | int = None,
     ) -> T_HUMANS:
         amount_of_groups = amount_of_groups or random.randint(5, 10)
         amount_of_humans = amount_of_humans or random.randint(3, 30)
@@ -55,7 +55,7 @@ class DataProvider:
         return self._generate_humans(groups=_groups, amount_of_humans=amount_of_humans)
 
 
-def organize_data(humans: T_HUMANS) -> dict[str:list[str]]:
+def organize_data(humans: T_HUMANS) -> dict[str : list[str]]:
     names_in_groups = {}
     for i in humans:
         names_in_groups.setdefault(i["group"], []).append(i["name"])
